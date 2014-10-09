5239d06987b63e19579f74ccba48c38c0aa420eb
Adam Townsend <desnudopenguino@gmail.com>
2014-10-09 15:48:41 -0700

    changed error message to email, not username

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -78,7 +78,7 @@ class UsersController extends AppController {
     		if ($this->Auth->login()) {
             	return $this->redirect($this->Auth->redirect());
 	        }
-    	    $this->Session->setFlash(__('Invalid username or password, try again'));
+    	    $this->Session->setFlash(__('Invalid email or password, try again'));
 	    }
 	}
 

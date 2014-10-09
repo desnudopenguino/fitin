2ae6815764547ff781ae07fd3ae7bea8240775d9
Adam Townsend <desnudopenguino@gmail.com>
2014-10-09 14:41:31 -0700

    added some validation rules, not quite sure if they apply to just registration or if they are for all times the fields are used

--- app/Model/User.php
+++ app/Model/User.php
@@ -1,5 +1,26 @@
 App::uses('AppModel', 'Model');
 
 Class User extends AppModel {
+	public $validate = array(
+		'email' => array(
+			'required' => array(
+				'rule' => array('notEmpty'),
+				'message' => "Email is blank" 
+
+		'password' => array(
+			'required' => array(
+				'rule' => array('notEmpty'),
+				'message' => "Password is blank"
+			)
+		),
+
+		'roleId' => array(
+			'required' => array(
+				'rule' => array('naturalNumber', true),
+				'message' => "Role is not valid"
+
+		)
+
+	);	
 
 }

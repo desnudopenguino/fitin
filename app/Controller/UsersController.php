399ab0749eb0eaf851d418e4e4edf98da698cfb1
Adam Townsend <desnudopenguino@gmail.com>
2014-10-10 11:16:20 -0700

    duplicated add function to register

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -4,7 +4,7 @@ App::uses('AppController', 'Controller');
 class UsersController extends AppController {
     public function beforeFilter() {
         parent::beforeFilter();
-        $this->Auth->allow('add', 'logout');
+        $this->Auth->allow('add', 'logout','register');
     }
 
 //index
@@ -24,6 +24,20 @@ class UsersController extends AppController {
 
 //add
     public function add() {
+        if ($this->request->is('post')) {
+            $this->User->create();
+            if ($this->User->save($this->request->data)) {
+                $this->Session->setFlash(__('The user has been saved'));
+                return $this->redirect(array('action' => 'index'));
+            }
+            $this->Session->setFlash(
+                __('The user could not be saved. Please, try again.')
+            );
+        }
+    }
+
+//register replaces add
+    public function register() {
         if ($this->request->is('post')) {
             $this->User->create();
             if ($this->User->save($this->request->data)) {

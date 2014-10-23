fc29aedf3a708d5d4dcf8957b8c24652375f225e
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-10-23 16:17:18 -0700

    forgot to delete a ]

--- app/Controller/ApplicantsController.php
+++ app/Controller/ApplicantsController.php
@@ -9,7 +9,7 @@ class ApplicantsController extends AppController {
 	}
 
 	function dashboard() {
-debug($this->Applicant->id = $this->Auth->user('id']));
+debug($this->Applicant->id = $this->Auth->user('id'));
 
 	}
 

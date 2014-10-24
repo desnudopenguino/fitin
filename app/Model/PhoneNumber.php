adad7925688938e7524991697d3cd5256f13620f
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-10-24 13:58:15 -0700

    trying to remove spaces in the phone number with str_replace

--- app/Model/PhoneNumber.php
+++ app/Model/PhoneNumber.php
@@ -23,9 +23,7 @@ Class PhoneNumber extends AppModel {
 	public function beforeSave($options = array()) {
 //filter the phone number down to just digits
 		if(isset($this->data[$this->alias]['phone_number'])) {
-			filter_var($this->data[$this->alias]['phone_number'], FILTER_SANITIZE_NUMBER_INT);
-			$tempPhoneNumber = explode('+',$this->data[$this->alias]['phone_number']);
-			
+			$this->data[$this->alias]['phone_number'] = str_replace(' ','', $this->data[$this->alias]['phone_number']);
 		}
 		return true;
 	}

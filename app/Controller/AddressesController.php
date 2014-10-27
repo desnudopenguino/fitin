3705cd912b3a16b9c3b91af6a2c3413905d8de15
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-10-27 12:22:44 -0700

    added address controller

---
+++ app/Controller/AddressesController.php
@@ -0,0 +1,23 @@
+<?php
+ App::uses('AppController', 'Controller');
+
+class AddressesController extends AppController {
+
+	public function add() {
+		$this->set('states',$this->Address->State->find('list', array('fields' => array('State.id','State.short_name'))));
+		if($this->request->is('post')) {
+			$this->Address->create();
+			if($this->Address->save($this->request->data)) {
+				$this->Session->setFlash(__('The address has been saved'));
+			}
+			else {
+				$this->Session->setFlash(__('The address could not be saved, please try again'));
+			}
+		}
+	}
+
+	public function edit() {
+
+	}
+ }
+?>

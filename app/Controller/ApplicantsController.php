501c4ec3b9fd3c44f75d7fbf2d7df4def1c78b60
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-10-28 13:27:59 -0700

    attempting to get the first state

--- app/Controller/ApplicantsController.php
+++ app/Controller/ApplicantsController.php
@@ -96,6 +96,7 @@ class ApplicantsController extends AppController {
 			throw new NotFoundException(__('Invalid User'));
 		}
 		debug($applicant);
+		$this->Applicant->User->Address->State->find('first');
 //		$state = $this->Applicant->User->Address->State->find('first',array('conditions' => array('id' => $applicant['Address']['state_id'])));
 //		$applicant['Address']['state'] = $state['State']['short_name'];
 		$this->set('applicant',$applicant);

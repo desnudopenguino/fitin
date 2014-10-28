0ea2694c037becf4055312ebe5fb877e873adf55
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-10-28 15:55:31 -0700

    find first of State

--- app/Controller/ApplicantsController.php
+++ app/Controller/ApplicantsController.php
@@ -98,7 +98,7 @@ class ApplicantsController extends AppController {
 		}
 		debug($applicant);
 //		$this->Applicant->User->Address->State->find('all');
-		debug($this->Applicant->User->Address->States);
+		debug($this->Applicant->User->Address->State->find('first'));
 		$this->set('applicant', $applicant);
 	}
  }

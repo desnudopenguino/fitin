5d6db269df0b659f07d6c3d8b7b1870e856a889c
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-10-28 12:52:28 -0700

    roleId not role_id

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -24,12 +24,11 @@ class UsersController extends AppController {
 				if(empty($user)) {
   	    	throw new NotFoundException(__('Invalid user'));
 				}
-debug($user);
-				switch($user['User']['role_id']) {
+				switch($user['User']['roleId']) {
 					case 1: // Employer
 						break;
 					case 2: // Applicant
-						//$this->redirect(array('controller' => 'applicants', 'action' => 'view',$url));
+						$this->redirect(array('controller' => 'applicants', 'action' => 'view',$url));
 						break;
 					case 3: // Recruiter
 						break;

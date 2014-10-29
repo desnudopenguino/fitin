098be51d6a437d574eeb19eefa2c2c5d7ee05774
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-10-29 16:13:47 -0700

    added ajax request logic, in anticipation for ajax calls.

--- app/Controller/CertificationsController.php
+++ app/Controller/CertificationsController.php
@@ -12,7 +12,10 @@ class CertificationsController extends AppController {
 			else {
 				$this->Session->setFlash(__('The certification could not be saved, please try again'));
 			}
+		} else if ($this->request->is('ajax')) {
+			$this->render('certification-ajax-response', 'ajax');
 		}
+
 	}
 
 	public function index() {

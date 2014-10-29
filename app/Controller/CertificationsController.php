46f06987735869fe5a6c9b8d2fd6fabd089e3917
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-10-29 10:50:18 -0700

    added certification controller with add function for now.

---
+++ app/Controller/CertificationsController.php
@@ -0,0 +1,18 @@
+<?php
+ App::uses('AppController', 'Controller');
+
+class CertificationsController extends AppController {
+
+	public function add() {
+		if($this->request->is('post')) {
+			$this->Certification->create();
+			if($this->Certification->save($this->request->data)) {
+				$this->Session->setFlash(__('The certification has been saved'));
+			}
+			else {
+				$this->Session->setFlash(__('The certification could not be saved, please try again'));
+			}
+		}
+	}
+ }
+?>

244c6cb5871a37575e961062a30863767d21ccfb
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-10-30 16:34:57 -0700

    seeing if the event is being called on the form submit

--- app/webroot/js/certification.js
+++ app/webroot/js/certification.js
@@ -1,10 +1,11 @@
 // call to save new certification
 $('#create-certification').submit(function( event ) {
-	$.ajax({
+	alert("submitted the form!");
+/*	$.ajax({
 		url: '/certifications/add',
 		data: $('#create-certification').serialize(),
 		success: function(result) {
 			alert(result);
-		}
+		}*/
 	return false;
 });

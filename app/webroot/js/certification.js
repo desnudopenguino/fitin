949350e5f17745c4be67c6a16f6c19781eed97ca
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-10-31 10:11:25 -0700

    added tbody, because the table has a tbody

--- app/webroot/js/certification.js
+++ app/webroot/js/certification.js
@@ -9,7 +9,7 @@ $('#createCertificationForm').submit(function( event ) {
 		data: $('#createCertificationForm').serialize(),
 		success: function(result) {
 			$('#createCertificationModal').modal('hide');
-			$('#certificationsTabel').append($(result).hide().fadeIn(1000));
+			$('#certificationsTabel tbody').append($(result).hide().fadeIn(1000));
 		}	
 	});
 	return false;

78ee556c41bdc922d8977117d006efd0ffbab4dd
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-11-03 14:54:24 -0800

    forgot the beforesave to save the item.

--- app/Model/Education.php
+++ app/Model/Education.php
@@ -5,5 +5,11 @@ Class Education extends AppModel {
 	public $belongsTo = array(
 		'Applicant','Degree','School','Concentration'
 	);
+
+	public function beforeSave($options = array()) {
+		$this->data[$this->alias]['applicant_id'] = AuthComponent::user('id');
+		return true;
+	}
+
 }
 ?>

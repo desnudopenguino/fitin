677608991135f32647918a5abbf544eea61bd1e1
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-11-03 15:38:34 -0800

    trying by avoiding hidden types

--- app/webroot/js/general.js
+++ app/webroot/js/general.js
@@ -1,5 +1,5 @@
 // autofocus on first visible field in modal
 $('.modal').on('shown.bs.modal', function () {
   lastfocus = $(this);
-  $(this).find('input:visible:enabled:first').focus();
+  $(this).find('input[type!=hidden]:first').focus();
 })

ac5dee3481cea5a171298ad3a4af1ea0a7cf7155
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-11-03 16:21:42 -0800

    i was using the .on call wrong, have to append it to the document to allow for stuff to work right.

--- app/webroot/js/certification.js
+++ app/webroot/js/certification.js
@@ -16,7 +16,9 @@ $('#createCertificationForm').submit(function() {
 });
 
 //call to delete certification
-$("form[id^='deleteCertification_']").on('submit', function() {
+//$("form[id^='deleteCertification_']").on('submit', function() {
+$(document).on('submit',"form[id^='deleteCertification_']", function() {
+console.log("it works yay");
 	var id = $(this).attr('id');
 	$.ajax({
 		url: $(this).attr('action'),

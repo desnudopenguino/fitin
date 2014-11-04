a1f09cb13c0db7192f7ce0314592f58a35467663
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-11-04 09:55:07 -0800

    changed some general js stuff. attempting to have datepicker work correctly nested inside modals. put everything in document.ready (so DOM is loaded first)

--- app/webroot/js/general.js
+++ app/webroot/js/general.js
@@ -1,8 +1,3 @@
-// autofocus on first visible field in modal
-$('.modal').on('shown.bs.modal', function () {
-  lastfocus = $(this);
-  $(this).find('input[type!=hidden],select').filter(':first').focus();
-})
 
 $( function() { 
 	$('input[name*="date"]').datepicker({
@@ -10,4 +5,16 @@ $( function() {
 		changeYear: true,
 		dateFormat: 'yy-mm-dd'
 	});
+
+	//attempt to keep modal focus while using datepicker.
+	var enforceModalFocusFn = $.fn.modal.Constructor.prototype.enforceFocus;
+	$.fn.modal.Constructor.prototype.enforceFocus = function() {};
+
+	// autofocus on first visible field in modal
+	$('.modal').on('shown.bs.modal', function () {
+  	lastfocus = $(this);
+  	$(this).find('input[type!=hidden],select').filter(':first').focus();
+	});
 });
+
+

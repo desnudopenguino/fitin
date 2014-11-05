ca506dce0b857cb7361d3743de3ca094095b8910
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-11-05 11:00:25 -0800

    added project controller based off cert controller. will be tweaked for projects

---
+++ app/Controller/ProjectsController.php
@@ -0,0 +1,81 @@
+<?php
+ App::uses('AppController', 'Controller');
+
+class CertificationsController extends AppController {
+
+	public $helpers = array('Js');
+
+	public function add() {
+		if($this->request->is('post')) {
+			$this->Certification->create();
+			if($this->Certification->save($this->request->data)) {
+				$this->Session->setFlash(__('The certification has been saved'),
+					'alert',
+					array(
+						'plugin' => 'BoostCake',
+						'class' => 'alert-success'
+				));
+			}
+			else {
+				$this->Session->setFlash(__('The certification could not be saved, please try again',
+					'alert',
+					array(
+						'plugin' => 'BoostCake',
+						'class' => 'alert-warning'
+				)));
+			}
+		}
+		if ($this->request->is('ajax')) {
+			$this->disableCache();		
+			$certification = $this->Certification->read(null, $this->Certification->id);
+			$this->set('certification', $certification);
+			$this->layout = false;
+			$this->render('/Elements/Certifications/row');
+		}
+	}
+
+	public function delete($id = null) {
+		$this->request->onlyAllow('post');
+		
+		$this->Certification->read(null,$id);
+
+		if(!$this->Certification->exists()) {
+			throw new NotFoundException(__('Invalid Certification'));
+		}
+		if($this->Certification->data['Certification']['applicant_id'] == $this->Auth->user('id')) {
+			if($this->Certification->delete()) {
+				if($this->request->is('ajax')) {
+					$this->disableCache();
+					$this->layout = false;
+				}
+			}
+		}
+	}
+
+	public function edit($id = null) {
+		$this->Certification->read(null,$id);
+
+		if(!$this->Certification->exists()) {
+			throw new NotFoundException(__('Invalid Certification'));
+		}
+
+		if($this->Certification->data['Certification']['applicant_id'] == $this->Auth->user('id')) {
+			if($this->request->is('post') || $this->request->is('put')) {
+				if($this->Certification->save($this->request->data['Certification'])) {
+					if($this->request->is('ajax')) {
+						$this->disableCache();
+						$this->layout= false;
+						$this->set('certification', $this->Certification->read(null, $this->Certification->id));	
+						$this->render('/Elements/Certifications/row');
+					}
+				}
+			}			
+		}
+
+	}
+
+	public function index() {
+		$this->set('certifications', $this->Certification->find('all'));
+	}
+ }
+?>

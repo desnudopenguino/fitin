16a6ce0517a0b0fe8b310a35cdeac24e0a70086c
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-11-10 15:23:18 -0800

    the virtual field won't work right for employer.

--- app/Model/Employer.php
+++ app/Model/Employer.php
@@ -14,14 +14,5 @@ Class Employer extends AppModel {
 	public $hasMany = array(
 		);
 
-	public $virtualFields = array(
-		'display_name' => 'Organization.organization_name');
-
-	public function checkDisplayName() {
-		if(empty($this->data[$this->alias]['display_name'])) {
-			$this->data[$this->alias]['display_name'] = $this->data['User']['email'];
-		} 
-	}
-
 }
 ?>

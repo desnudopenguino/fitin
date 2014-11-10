7ab571d650c458702a44e83f963467ded31bd78d
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-11-10 13:28:51 -0800

    forgot a .

--- app/webroot/js/project.js
+++ app/webroot/js/project.js
@@ -1,6 +1,6 @@
 // call to save new project
 $('#createProjectForm').submit(function() {
-console.log(	$('#createProjectForm').find('#ProjectIndustry1IndustryId').is(':hidden')remove());
+console.log(	$('#createProjectForm').find('#ProjectIndustry1IndustryId').is(':hidden').remove());
 console.log(	$('#createProjectForm').find('#ProjectIndustry2IndustryId').is(':hidden').remove());
 	$.ajax({
 		url: $(this).attr('action'),

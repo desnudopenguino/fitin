e25013f367716503c1fa2d34e0c6ae78c61e9ea0
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-11-10 15:13:51 -0800

    added lots of bits to the employer model

--- app/Model/Employer.php
+++ app/Model/Employer.php
@@ -3,5 +3,25 @@ App::uses('AppModel', 'Model');
 
 Class Employer extends AppModel {
 
+	public $belongsTo = array(
+		'User','Organization' );
+
+	public $primaryKey = 'user_id';
+
+	public $hasOne = array(
+		);
+
+	public $hasMany = array(
+		);
+
+	public $virtualFields = array(
+		'display_name' => 'Employer.Organization.organization_name');
+
+	public function checkDisplayName() {
+		if(empty($this->data[$this->alias]['display_name'])) {
+			$this->data[$this->alias]['display_name'] = $this->data['User']['email'];
+		} 
+	}
+
 }
 ?>

9ac467d05e241d9763aa4662c85705b6bbda07a0
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-11-11 13:11:35 -0800

    added position model.

---
+++ app/Model/Position.php
@@ -0,0 +1,23 @@
+<?php
+App::uses('AppModel', 'Model');
+
+Class Position extends AppModel {
+
+	public $recursive = 2;
+	public $belongsTo = array(
+		'Employer',
+		'Degree'
+	);
+
+	public $hasMany = array(
+		'PositionIndustry',
+		'PositionFunction',
+		'PositionSkill'
+	);
+
+	public function beforeSave($options = array()) {
+		$this->data[$this->alias]['employer_id'] = AuthComponent::user('id');
+		return true;
+	}
+}
+?>

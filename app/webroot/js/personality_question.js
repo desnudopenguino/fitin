0ad0f58d0ad7ea2fed4d78af3747791f15a7c6ba
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-11-12 13:39:47 -0800

    added personality question js

---
+++ app/webroot/js/personality_question.js
@@ -0,0 +1,33 @@
+function loadRandomPersonalityQuestion() {
+	$.ajax({
+		url: '../personality_questions/random',
+		type: 'POST',
+		async: true,
+		success: function(result) {
+			$('#personalityContent').html(result);
+		}
+	});
+
+}
+//call to load personality question 
+$(document).on('click',"#personalityQuestions", function() {
+loadRandomPersonalityQuestion();
+	return false;
+});
+
+$(document).on('click','.personality-choice', function() {
+	$('#answer').val($(this).attr('value'));
+});
+
+$(document).on('submit','#saveUserPersonalityAnswerForm', function() {
+	$.ajax({
+		url: $(this).attr('action'),
+		type: $(this).attr('method'),
+		async: true,
+		data: $(this).serialize(),
+		success: function(result) {
+			loadRandomPersonalityQuestion();
+		}
+	});
+	return false;	
+});

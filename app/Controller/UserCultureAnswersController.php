903b686f4fe70257d8f964496ec68493664ba0d8
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-11-12 11:53:10 -0800

    added user culture answers controller

---
+++ app/Controller/UserCultureAnswersController.php
@@ -0,0 +1,10 @@
+<?php
+ App::uses('AppController', 'Controller');
+
+class UserCultureAnswersController extends AppController {
+
+	public function add() {
+debug($this->request->data);
+	}
+ }
+?>

4e832c1a54ce8a64cc9921202186a80487e6ac27
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-11-12 09:57:13 -0800

    added culture question controller and random action to get a random question.

---
+++ app/Controller/CultureQuestionsController.php
@@ -0,0 +1,11 @@
+<?php
+ App::uses('AppController', 'Controller');
+
+class CultureQuestionsController extends AppController {
+
+	public function random() {
+		$this->set('question', $this->CultureQuestion->find('first',array(
+			'order' => array('rand()'))));
+	}
+ }
+?>

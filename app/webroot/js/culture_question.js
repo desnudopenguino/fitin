94715af43678fad8cd954fa642a2cfada39448ab
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-11-12 10:26:31 -0800

    added culture question js

---
+++ app/webroot/js/culture_question.js
@@ -0,0 +1,13 @@
+//call to load culture question 
+$(document).on('click',"#cultureQuestions", function() {
+	$.ajax({
+		url: '../../culture_questions/random,
+		type: 'POST',
+		async: true,
+		success: function(result) {
+console.log("Load the culture question!");
+			$('#cultureContent').replace(result);
+		}
+	});
+	return false;
+});

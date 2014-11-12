55e5ec4c7f7d5ee2c3124ed0d7bcd7f32b81ff22
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-11-12 10:21:37 -0800

    added if request is ajax logic to load only the question data, to be loaded by the culture view.

--- app/Controller/CultureQuestionsController.php
+++ app/Controller/CultureQuestionsController.php
@@ -6,6 +6,10 @@ class CultureQuestionsController extends AppController {
 	public function random() {
 		$this->set('question', $this->CultureQuestion->find('first',array(
 			'order' => array('rand()'))));
+		if($this->request->is('ajax')) {
+			$this->disableCache();
+			$this->layout = false;
+		}
 	}
  }
 ?>

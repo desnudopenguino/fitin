5350dba47cb93f08a453eee8ea1b8984888df760
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-11-12 13:20:33 -0800

    great success! and cleaned up console logs since it works nicely now.

--- app/webroot/js/culture_question.js
+++ app/webroot/js/culture_question.js
@@ -4,7 +4,6 @@ function loadRandomCultureQuestion() {
 		type: 'POST',
 		async: true,
 		success: function(result) {
-console.log("Load the culture question!");
 			$('#cultureContent').html(result);
 		}
 	});
@@ -12,33 +11,21 @@ console.log("Load the culture question!");
 }
 //call to load culture question 
 $(document).on('click',"#cultureQuestions", function() {
-/*	$.ajax({
-		url: '../culture_questions/random',
-		type: 'POST',
-		async: true,
-		success: function(result) {
-console.log("Load the culture question!");
-			$('#cultureContent').html(result);
-		}
-	});*/
 loadRandomCultureQuestion();
 	return false;
 });
 
 $(document).on('click','.culture-choice', function() {
 	$('#answer').val($(this).attr('value'));
-console.log($(this) + " has been clicked");
 });
 
 $(document).on('submit','#saveUserCultureAnswerForm', function() {
-console.log("form submitting!");
 	$.ajax({
 		url: $(this).attr('action'),
 		type: $(this).attr('method'),
 		async: true,
 		data: $(this).serialize(),
 		success: function(result) {
-console.log("form success!");
 			loadRandomCultureQuestion();
 		}
 	});

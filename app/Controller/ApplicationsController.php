5c81462979fd3357472c36028da09feec1305aba
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-11-13 14:43:18 -0800

    added application controller

---
+++ app/Controller/ApplicationsController.php
@@ -0,0 +1,22 @@
+<?php
+ App::uses('AppController', 'Controller');
+
+class ApplicationsController extends AppController {:
+
+	public function add() {
+		if($this->request->is('post')) {
+			$this->Application->create();
+			if($this->Application->save($this->request->data)) {
+				$this->Session->setFlash(__('The address has been saved'));
+			}
+			else {
+				$this->Session->setFlash(__('The address could not be saved, please try again'));
+			}
+		}
+	}
+
+	public function edit() {
+
+	}
+ }
+?>

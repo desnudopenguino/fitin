101ec9eed8219969d2396e1c77c671689baf96b0
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-11-13 14:33:02 -0800

    added dashboard js fon functions associated with the dashboard

---
+++ app/webroot/js/dashboard.js
@@ -0,0 +1,22 @@
+//load the mail stuff if inbox button is clicked
+$(document).on('click',"#inbox-btn", function() {
+	$.ajax({
+		url: 'messages/inbox',
+		type: 'GET',
+		async: true,
+		success: function(result) {
+			$('#dashboardContent').html(result);
+		}});
+});
+
+//load the applications stuff if applications button is clicked
+$(document).on('click',"#applications-btn", function() {
+	$.ajax({
+		url: 'applications/index',
+		type: 'GET',
+		async: true,
+		success: function(result) {
+			$('#dashboardContent').html(result);
+		}});
+
+});

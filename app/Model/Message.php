7314bce46f36751c131f56fc610da60e85cc06d0
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-11-13 10:59:34 -0800

    added message model.

---
+++ app/Model/Message.php
@@ -0,0 +1,22 @@
+<?php
+App::uses('AppModel', 'Model');
+
+Class Message extends AppModel {
+
+	public $belongsTo = array(
+		'User' => array(
+			'className' => 'User',
+			'foreignKey' => 'sender_id'
+		),
+		'User' => array(
+			'className' => 'State',
+			'foreignKey' => 'receiver_id'
+		)
+	);
+
+	public function beforeSave($options = array()) {
+		$this->data[$this->alias]['sender_id'] = AuthComponent::user('id');
+		return true;
+	}
+}
+?>

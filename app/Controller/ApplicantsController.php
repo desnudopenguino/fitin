76f2a7ac6d7d2c227a5990aa8ca929a4bbc5ea99
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-11-14 10:41:56 -0800

    added applicant object to search action

--- app/Controller/ApplicantsController.php
+++ app/Controller/ApplicantsController.php
@@ -92,6 +92,9 @@ class ApplicantsController extends AppController {
 
 // Search - search page, applicant gets matched up with open positions based on skills & culture match
 	public function search() {
+		$this->set('applicant', $this->Applicant->find('first', array(
+			'corditions' => array(
+				'Applicant.user_id' => $this->Auth->user('id')))));
 
 	}
 

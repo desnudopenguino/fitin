1ab16f8bfc079b7f82dd81b08f9c53402c3a1a36
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-11-14 14:30:26 -0800

    hopefully the code will spit out something useful, the function to compare cultures is "done"

--- app/Model/UserCultureAnswer.php
+++ app/Model/UserCultureAnswer.php
@@ -12,20 +12,32 @@ Class UserCultureAnswer extends AppModel {
 		return true;	
 	}
 
+//function that compares the culture, applicant id goes first, then employer id. this will return an array with the final stats (total # questions, matched # questions, and average in %)
 	public function compareCulture($applicantId, $employerId) {
-			$applicantCulture = $this->find('all', array(
-					'conditions' => array(
-						'UserCultureAnswer.user_id' => $applicantId),
-					'fields' => array(
-						'UserCultureAnswer.culture_question_id','UserCultureAnswer.culture_question_answer_id')));
-			 $employerCulture = $this->find('all', array(
-					'conditions' => array(
-						'UserCultureAnswer.user_id' => $employerId),
-					'fields' => array(
-						'UserCultureAnswer.culture_question_id','UserCultureAnswer.culture_question_answer_id')));
+		$applicantCulture = $this->find('all', array(
+			'conditions' => array(
+				'UserCultureAnswer.user_id' => $applicantId),
+			'fields' => array(
+				'UserCultureAnswer.culture_question_id','UserCultureAnswer.culture_question_answer_id')));
+		$employerCulture = $this->find('all', array(
+			'conditions' => array(
+				'UserCultureAnswer.user_id' => $employerId),
+			'fields' => array(
+				'UserCultureAnswer.culture_question_id','UserCultureAnswer.culture_question_answer_id')));
+		$count = 0;
+		$totalQuestions = $matches = $average = 0;
+		foreach($employerCulture as $qkey => $question) {
+			$totalQuestions++;
+			foreach($applicantCulture as $aKey => $answer) {
+				count++;
+				if($question['UserCultureAnswer']['culture_question_id'] == $answer['UserCultureAnswer']['culture_question_id'] && $question['UserCultureAnswer']['culture_question_answer_id'] == $answer['UserCultureAnswer']['culture_question_answer_id']) {
+					$matches ++;
+					unset($applicantCulture[$aKey]);
+			}
+			
+		}
 
-
-		return array($applicantCulture, $employerCulture);
+		return array('total' => $totalQuestions, 'match' => $matches, 'percent' => $average, 'iterations' => $count);
 	}
 }
 ?>

cc31b4dbc2e2d441e3fd59130b4c7a21d1f51385
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-11-14 13:36:08 -0800

    moved stuff into block checking if logged in user is applicant

--- app/Controller/EmployersController.php
+++ app/Controller/EmployersController.php
@@ -123,18 +123,23 @@ class EmployersController extends AppController {
 			'conditions' => array(
 				'Position.employer_id' => $employer['User']['id']))));
 
+		$myId = $this->Employer->User->find('first', array(
+			'conditions' => array(
+				'User.id' => $this->Auth->user('id')),
+			'fields' => array(
+				'User.roleId')));
+
+		$myId = $myId['User']['roleId'];
+
+		if($myId == 2) { //i'm an applicant!
+		$this->set('myId', $myId);
 		$this->set('employer_culture', $this->UserCultureAnswer->find('all', array(
 			'conditions' => array(
 				'UserCultureAnswer.user_id' => $employer['User']['id']),
 			'fields' => array(
 				'UserCultureAnswer.culture_question_id','UserCultureAnswer.culture_question_answer_id'))));
-
-			$myId = $this->Employer->User->find('first', array(
-				'conditions' => array(
-					'User.id' => $this->Auth->user('id')),
-				'fields' => array(
-					'User.roleId')));
-debug($myId['User']['roleId']);
+debug($myId);
+		}
 	}
 }
 ?>

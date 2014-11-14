a8d590125cbe3b0fe292a6d21bb5ab11fb0988fd
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-11-14 10:38:46 -0800

    added attributes to applicant data card

--- app/Model/ApplicantDataCard.php
+++ app/Model/ApplicantDataCard.php
@@ -7,5 +7,14 @@ Class ApplicantDataCard extends AppModel {
 
 	public $useTable = false;
 
+	public $education = array(); //list of educations
+
+	public $certification = array(); //list of certifications;
+
+	public $work_experience = array( 
+		'industries' => array(),
+		'functions' => array(),
+		'skills' => array()); //framework for measurable job experience
+
 }
 ?>

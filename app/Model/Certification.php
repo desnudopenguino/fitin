d5f600c035fc098275955b8868b4787dbd87a754
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-11-17 10:17:47 -0800

    shortened function names, and made them make sense, while shortened

--- app/Model/Certification.php
+++ app/Model/Certification.php
@@ -9,13 +9,20 @@ public $actsAs = array('Containable');
 		'Applicant'
 		);
 
-	public function loadActiveApplicantCertifications($applicant_id) {
+	public function loadApplicantActive($applicant_id) {
 		return $this->find('all', array(
 			'conditions' => array(
 				'Certification.applicant_id' => $applicant_id,
 				'OR' => array(
 					'Certification.expiration_date IS NOT NULL',
 					'Certification.expiration_date >' => date('Y-m-d'))),
+			'contain' => false));
+	}
+
+	public function loadApplicantAll($applicant_id) {
+		return $this->find('all', array(
+			'conditions' => array(
+				'Certification.applicant_id' => $applicant_id)
 			'contain' => false));
 	}
 }

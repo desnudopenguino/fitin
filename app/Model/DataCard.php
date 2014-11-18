08cda79656ad354c44da3bdeecc4b8458e8ea355
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-11-17 16:56:25 -0800

    renamed compare function

--- app/Model/DataCard.php
+++ app/Model/DataCard.php
@@ -5,7 +5,7 @@ Class DataCard extends AppModel {
 
 	public $useTable = false;
 
-	public function compareDataCards($applicant_card, $position_card) {
+	public function compare($applicant_card, $position_card) {
 		debug(array('ApplicantCard' => $applicant_card,
 			'PositionCard' => $position_card));
 	}

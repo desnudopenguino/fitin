327ca1fa2fae935067630392d16c5f017b71b191
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-11-18 12:27:52 -0800

    added employer search page js

---
+++ app/webroot/js/employer_search.js
@@ -0,0 +1,11 @@
+//load the mail stuff if inbox button is clicked
+$(document).on('submit',"#searchPositionForm", function() {
+	$.ajax({
+		url: $(this).attr('action'),
+		type: $(this).attr('method'),
+		async: true,
+		success: function(result) {
+			$('#results').html(result).hide().fadeIn(300);
+console.log('successful search!');
+		}});
+});

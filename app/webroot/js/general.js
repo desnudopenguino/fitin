e19b0974f9282a71b2bd5fcaacdad824f3897368
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-11-19 15:23:53 -0800

    removed code for datepicker

--- app/webroot/js/general.js
+++ app/webroot/js/general.js
@@ -1,21 +1,7 @@
-
 $( function() { 
-	//attempt to keep modal focus while using datepicker.
-	var enforceModalFocusFn = $.fn.modal.Constructor.prototype.enforceFocus;
-	$.fn.modal.Constructor.prototype.enforceFocus = function() {};
-
 	// autofocus on first visible field in modal
 	$('.modal').on('shown.bs.modal', function () {
   	lastfocus = $(this);
   	$(this).find('input[type!=hidden],select').filter(':first').focus();
-	});
-});
-
-//on focus on a date input, load date picker
-$(document).on('focus',"input[name*='date']", function() {
-	$(this).datepicker({
-		changeMonth: true,
-		changeYear: true,
-		dateFormat: 'yy-mm-dd'
 	});
 });

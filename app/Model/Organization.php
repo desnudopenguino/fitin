3c3daf567639f94f2b68378da13f7fd00347bf4b
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-11-20 14:00:23 -0800

    added findByName function to find the organization by its name, added checkAndCreate that is called when creating an associated object with an organization related to it.

--- app/Model/Organization.php
+++ app/Model/Organization.php
@@ -5,5 +5,21 @@ Class Organization extends AppModel {
 	public $belongsTo = array('OrganizationType');
 
 	public $hasMany = array('Project');
+
+	public function findByName($organization_name) {
+		return $this->find('first' array(
+			'conditions' => array(
+				'Organization.organization_name' => $organization_name)));
+	}
+
+	public function checkAndCreate($data) {
+		if($organization = $this->findByName($data['Organization']['organization_name'])) {
+		} else {
+			$this->create();
+			$this->save($data);
+			$organization = $this->findByName($data['Organization']['organization_name']);
+		}
+		return $organization;
+	}
 }
 ?>

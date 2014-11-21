19646923953fa1e58e856f4f56fd4b7e808ce336
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-11-21 11:00:10 -0800

    cleaned up application model & controller

--- app/Controller/ApplicationsController.php
+++ app/Controller/ApplicationsController.php
@@ -20,19 +20,13 @@ class ApplicationsController extends AppController {
 	}
 
 	public function applicantIndex() {
-		$this->set('applications', $this->Application->find('all', array(
-			'conditions' => array(
-				'Application.applicant_id' => $this->Auth->user('id')))));
+		$this->set('applications', $this->Application->findApplicant($this->Auth->user('id')));
 		if($this->request->is('ajax')) {
 			$this->layout = false;
 		}
 	}
 	public function employerIndex() {
-		$this->set('applications', $this->Application->Position->find('all', array(
-			'conditions' => array(
-				'Position.employer_id' => $this->Auth->user('id')),
-			'contain' => array(
-				'Application'))));
+		$this->set('applications', $this->Application->findEmployer($this->Auth->user('id')));
 	}
  }
 ?>
--- app/Model/Application.php
+++ app/Model/Application.php
@@ -11,5 +11,19 @@ Class Application extends AppModel {
 	public function beforeSave($options = array()) {
 		return true;
 	}
+
+	public function findApplicant($applicant_id = null) {
+		return $this->find('all', array(
+			'conditions' => array(
+				'Application.applicant_id' => $applicant_id)));
+	}
+	
+	public function findEmployer($employer_id = null) {
+		return $this->find('all', array(
+			'contain' => array(
+				'Employer' =>
+					'conditions' => array(
+						'Employer.user_id' => $employer_id))));
+	}
 }
 ?>

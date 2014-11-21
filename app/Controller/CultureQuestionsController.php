04c68beb39765465a9ed11d53a771fbb09ea9162
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-11-21 13:28:02 -0800

    added logic to make sure it is a post request.

--- app/Controller/CultureQuestionsController.php
+++ app/Controller/CultureQuestionsController.php
@@ -15,12 +15,13 @@ class CultureQuestionsController extends AppController {
 	}
 
 	public function undo() {
-		$last_answer = $this->UserQuestionAnswer->findLastUserAnswer($this->Auth->user('id'));
-debug($last_answer);
-		$this->CultureQuestion->UserQuestionAnswer->id = $last_answer['UserQuestionAnswer']['id'];
-		$this->CultureQuestion->UserQuestionAnswer->delete();
-		$this->set('question', $this->CultureQuestion->findById($last_answer['UserCultureAnswer']['culture_question_id']));
-		
+		if($this->requesti->is('post')) {
+			$last_answer = $this->UserQuestionAnswer->findLastUserAnswer($this->Auth->user('id'));
+	debug($last_answer);
+			$this->CultureQuestion->UserQuestionAnswer->id = $last_answer['UserQuestionAnswer']['id'];
+			$this->CultureQuestion->UserQuestionAnswer->delete();
+			$this->set('question', $this->CultureQuestion->findById($last_answer['UserCultureAnswer']['culture_question_id']));
+		}	
 		if($this->request->is('ajax')) {
 			$this->disableCache();
 			$this->layout = false;

18070f828379671036194dd86e4ff86fe3d58c0a
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-11-21 13:24:17 -0800

    added undo function.
    Signed-off-by: Bucky Townsend <atownsend@unluckysandpiper.com>

--- app/Controller/CultureQuestionsController.php
+++ app/Controller/CultureQuestionsController.php
@@ -11,6 +11,21 @@ class CultureQuestionsController extends AppController {
 			$this->disableCache();
 			$this->layout = false;
 		}
+		$this->render('question');
+	}
+
+	public function undo() {
+		$last_answer = $this->UserQuestionAnswer->findLastUserAnswer($this->Auth->user('id'));
+debug($last_answer);
+		$this->CultureQuestion->UserQuestionAnswer->id = $last_answer['UserQuestionAnswer']['id'];
+		$this->CultureQuestion->UserQuestionAnswer->delete();
+		$this->set('question', $this->CultureQuestion->findById($last_answer['UserCultureAnswer']['culture_question_id']));
+		
+		if($this->request->is('ajax')) {
+			$this->disableCache();
+			$this->layout = false;
+		}
+		$this->render('question');
 	}
  }
 ?>

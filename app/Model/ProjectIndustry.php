53759a37ef05a47fb0513488373c4fdc71299c30
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-11-24 13:53:41 -0800

    function wasn't inside the class!

--- app/Model/ProjectIndustry.php
+++ app/Model/ProjectIndustry.php
@@ -5,11 +5,11 @@ Class ProjectIndustry extends AppModel {
 	public $belongsTo = array(
 		'Project',
 		'Industry' );
-}
 
 	public function beforeSave($options = array()) {
 		if(empty($this->data['ProjectIndustry']['industry_id'])) {
 			return false;
 		} else { return true; }
 	}
+}
 ?>

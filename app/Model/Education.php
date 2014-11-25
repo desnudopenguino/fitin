806fdbdd58a14c7e388c56043bce8010a055f9ae
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-11-24 16:36:15 -0800

    this should fix it!

--- app/Model/Education.php
+++ app/Model/Education.php
@@ -44,15 +44,15 @@ Class Education extends AppModel {
 				'Degree',
 				'Organization',
 				'Industry')));
-/*
+
 		$education['Education']['Degree'] = $education['Degree'];
 		unset($education['Degree']);
 		$education['Education']['Organization'] = $education['Organization'];
 		unset($education['Organization']);
 		$education['Education']['Industry'] = $education['Industry'];
 		unset($education['Industry']);
-*/
-		return $education;	
+
+		return $education['Education'];	
 	}
 	
 }

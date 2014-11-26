e75da7147a72ea5fafaebf187c8739bfbef52aa0
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-11-26 11:09:53 -0800

    tweaking to build the functionality for project skills

--- app/Model/ProjectSkill.php
+++ app/Model/ProjectSkill.php
@@ -5,5 +5,24 @@ Class ProjectSkill extends AppModel {
 	public $belongsTo = array(
 		'Project',
 		'Skill' );
+
+	public function findByName($skill_name) {
+		return $this->find('first', array(
+			'conditions' => array(
+				'Skill.skill_name' => $skill_name)));
+	}
+
+	public function checkAndCreateSkills($data) {
+		$this->explodeString($data);
+debug($data);
+	}
+
+	public function explodeString($data) {
+		$skills = explode(', ', $data['ProjectSkill']['skill_names']);
+
+		foreach($skills as $skill) {
+			$data['ProjectSkill'][]['skill_name'] = $skill;
+		}
+	}
 }
 ?>

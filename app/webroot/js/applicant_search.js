16e1fde9fc337fdcfd47d4d93ba737dbe9b44110
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-11-26 17:04:21 -0800

    preventing the default of the click action

--- app/webroot/js/applicant_search.js
+++ app/webroot/js/applicant_search.js
@@ -1,6 +1,8 @@
 //load the mail stuff if inbox button is clicked
-$(document).on('click',".apply", function() {
+$(document).on('click',".apply", function(e) {
+
 console.log('Applied to '+ $(this).attr('href'));
+e.preventDefault();
 	$.ajax({
 		url: $(this).attr('href'),
 		type: 'POST',

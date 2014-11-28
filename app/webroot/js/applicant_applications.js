46c3b42b926916f869e5f2eac2b0d400e9ddb82b
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-11-28 15:00:23 -0800

    ready to remove the application from the list.

--- app/webroot/js/applicant_applications.js
+++ app/webroot/js/applicant_applications.js
@@ -1,14 +1,12 @@
 //load the mail stuff if inbox button is clicked
 $(document).on('click',".cancel", function(e) {
-
-var id = '#'+$(this).attr('id');
-console.log(id);
+var id = $(this).attr('href').match(/\d+/g,'');
 	$.ajax({
 		url: $(this).attr('href'),
 		type: 'POST',
 		async: true,
 		success: function() {
-console.log('cancelled to '+ $(this).attr('href'));
+			$('#application_'+id).fadeOut(200, function() ( $(this).remove(); });
 		}});
 	return false;
 });

ca5f4520a50d0a55f6e7b51f1a344e5426f73d42
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-11-28 15:22:01 -0800

    changed js for new application stuff

--- app/webroot/js/applicant_dashboard.js
+++ app/webroot/js/applicant_dashboard.js
@@ -13,7 +13,7 @@ console.log('load the inbox successful');
 //load the applications stuff if applications button is clicked
 $(document).on('click',"#applications-btn", function() {
 	$.ajax({
-		url: '../applications/applicantIndex',
+		url: '../applications/applicant',
 		type: 'GET',
 		async: true,
 		success: function(result) {

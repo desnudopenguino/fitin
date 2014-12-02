b207d817bffc2910dccba2811486be979c70d73e
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-12-01 18:57:32 -0800

    changed js to toggle.
    Signed-off-by: Bucky Townsend <atownsend@unluckysandpiper.com>

--- app/webroot/js/message.js
+++ app/webroot/js/message.js
@@ -1,5 +1,5 @@
 //load the mail stuff if inbox button is clicked
 $(document).on('click',".message", function() {
-//	$($(this).data('target')).toggle('slow');
+	$($(this).data('target')).toggle('slow');
 console.log('show/hide the item');
 });

b737aab52dbdf3daeec3e700b5ac6d385e936595
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-12-03 09:23:07 -0800

    made a copy of the applicant applications js to employer applications

---
+++ app/webroot/js/employer_applications.js
@@ -0,0 +1,14 @@
+//load the mail stuff if inbox button is clicked
+$(document).on('click',".cancel", function() {
+var href = $(this).attr('href');
+var id = href.match(/\d+/g,'');
+	$.ajax({
+		url: $(this).attr('href'),
+		type: 'POST',
+		async: true,
+		success: function() {
+			$('#application_'+id).fadeOut(200, function() { $(this).remove(); });
+		}});
+	return false;
+});
+

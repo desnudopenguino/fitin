4c398cb8df0a9c98bd6d47078c8591bde9b66ef3
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-12-03 14:23:33 -0800

    added debugs to see if buttons actually work

--- app/webroot/js/message.js
+++ app/webroot/js/message.js
@@ -5,13 +5,14 @@ console.log('show/hide the item');
 });
 
 $(document).on('click','#sent', function() {
-
+console.log("view sent messages");
 });
 
 $(document).on('click','#archive', function() {
+console.log("view archived messages");
 
 });
 
 $(document).on('click','#inbox', function() {
-
+console.log("view inbox messages");
 });

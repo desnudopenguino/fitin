4c59feeaae40b6346a899dabdc2b626ea0244ea9
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-12-10 17:23:50 -0800

    user didn't work

--- app/Model/Applicant.php
+++ app/Model/Applicant.php
@@ -173,7 +173,7 @@ Class Applicant extends AppModel {
 
 	public function afterFind($results, $primayr = false) {
 		foreach($results as $key => $result) {
-debug($result['User']);
+debug($result);
 /*			if(empty($result['Applicant']['display_name'])) {
 				$results[$key]['Applicant']['display_name'] = $result['User']['email'];
 			}*/

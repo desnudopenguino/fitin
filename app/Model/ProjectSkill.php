38320a46f1f72db41e97c10ba715418d7b81bc12
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-12-12 15:55:32 -0800

    moved things around a littl.

--- app/Model/ProjectSkill.php
+++ app/Model/ProjectSkill.php
@@ -29,18 +29,17 @@ debug($skills);
 		}
 		foreach($data['ProjectSkill'] as $sKey => $project_skill) {
 			$skill = $this->Skill->checkAndCreate($project_skill);
-			unset($data['ProjectSkill'][$sKey]['Skill']);
 			if(!in_array($skill['Skill']['id'], $skills)) {
 				$data['ProjectSkill'][$sKey]['skill_id'] = $skill['Skill']['id'];
 				unset($skills[array_search($skill['Skill']['id'],$skills)]);
 			} else {
 				unset($skills[array_search($skill['Skill']['id'],$skills)]);
 			}
-
-			foreach($skills as $key =>$skill) {
-				$this->id = $key;
-				$this->delete();
-			}
+//			unset($data['ProjectSkill'][$sKey]['Skill']);
+		}
+		foreach($skills as $key =>$skill) {
+			$this->id = $key;
+			$this->delete();
 		}
 		return $data;
 	} 

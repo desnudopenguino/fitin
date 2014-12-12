264af08b34c43a85f5ec437816e436e6fa80b3ec
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-12-12 15:44:08 -0800

    uncommented some code

--- app/Model/ProjectSkill.php
+++ app/Model/ProjectSkill.php
@@ -29,12 +29,12 @@ debug($skills);
 		}
 		foreach($data['ProjectSkill'] as $sKey => $project_skill) {
 			$skill = $this->Skill->checkAndCreate($project_skill);
-/*			unset($data['ProjectSkill'][$sKey]['Skill']);
+			unset($data['ProjectSkill'][$sKey]['Skill']);
 			if(!in_array($skill['Skill']['id'], $skills)) {
 				$data['ProjectSkill'][$sKey]['skill_id'] = $skill['Skill']['id'];
 				unset($skills[array_search($skill['Skill']['id'],$skills)]);
 			}else {
-				unset($skills[array_search($skill['Skill']['id'],$skills)]);
+//				unset($skills[array_search($skill['Skill']['id'],$skills)]);
 			}
 /*
 			foreach($skills as $key =>$skill) {

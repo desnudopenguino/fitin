eee7312f5b278283846f2f6f798fdf6ad525c78c
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-12-12 15:12:15 -0800

    added logic to check if skill is already in project skills. doesn't add current project skills, adds new ones, and removes ones that are no longer in the project skills.

--- app/Model/ProjectSkill.php
+++ app/Model/ProjectSkill.php
@@ -22,6 +22,7 @@ Class ProjectSkill extends AppModel {
 	public function checkAndCreate($data) {
 		$data = $this->explode($data);
 debug($data);
+		$skills = array();
 		if(isset($data['Project']['id'])) {
 			$skills = $this->findProjectSkills($data['Project']['id']);
 debug($skills);
@@ -29,7 +30,17 @@ debug($skills);
 		foreach($data['ProjectSkill'] as $sKey => $project_skill) {
 			$skill = $this->Skill->checkAndCreate($project_skill);
 			unset($data['ProjectSkill'][$sKey]['Skill']);
-			$data['ProjectSkill'][$sKey]['skill_id'] = $skill['Skill']['id'];
+			if(!in_array($skill['Skill']['id'], $skills)) {
+				$data['ProjectSkill'][$sKey]['skill_id'] = $skill['Skill']['id'];
+				unset($skills[array_search($skill['Skill']['id'],$skills)]);
+			}else {
+				unset($skills[array_search($skill['Skill']['id'],$skills)]);
+			}
+
+			foreach($skills as $key =>$skill) {
+				$this->id = $key;
+				$this->delete();
+			}
 		}
 		return $data;
 	} 

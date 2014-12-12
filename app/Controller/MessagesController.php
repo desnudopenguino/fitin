9d520e4130ce2b9f85566e3b36ada34532ca5470
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-12-12 12:31:40 -0800

    message successful submission redirects to dashboard

--- app/Controller/MessagesController.php
+++ app/Controller/MessagesController.php
@@ -11,16 +11,7 @@ class MessagesController extends AppController {
 		
 		if($this->request->is('post') && !empty($this->request->data['Message']['message'] )) {
 			if($this->Message->save($this->request->data)) {
-				$this->Session->setFlash(__('The message has been sent'),
-					'alert', array(
-						'plugin' => 'BoostCake',
-						'class' => 'alert-success'));
-			}
-			else{
-				$this->Session->setFlash(__('The message has not been sent'),
-					'alert', array(
-						'plugin' => 'BoostCake',
-						'class' => 'alert-danger'));
+				$this->redirect(array('controller' => 'users', 'action' => 'dashboard'));
 			}
 		}
 	}

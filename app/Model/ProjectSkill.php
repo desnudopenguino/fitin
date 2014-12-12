c274900e92a97fab20456640abdc29539f9ba39a
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-12-12 12:57:55 -0800

    set it so that , won't be saved either

--- app/Model/ProjectSkill.php
+++ app/Model/ProjectSkill.php
@@ -20,7 +20,7 @@ Class ProjectSkill extends AppModel {
 		$data = $this->explode($data);
 
 		foreach($data['ProjectSkill'] as $sKey => $project_skill) {
-			if(!ctype_space($project_skill) && $project_skill != '') {
+			if(!ctype_space($project_skill) && $project_skill != '' && $project_skill != ',') {
 				$skill = $this->Skill->checkAndCreate($project_skill);
 				unset($data['ProjectSkill'][$sKey]['Skill']);
 				$data['ProjectSkill'][$sKey]['skill_id'] = $skill['Skill']['id'];

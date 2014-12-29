4a3a311c65d97933f9d6133a0afc831e93a2906a
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-12-29 14:39:27 -0800

    read referral from session

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -314,7 +314,7 @@ class UsersController extends AppController {
 		if(!empty($user)) {
 			$this->Session->write('referral',$user['User']['id']);
 		}
-debug($this->Session);
+debug($this->Session->read('referral'));
 		$this->autoRender = false;
 	}
 }

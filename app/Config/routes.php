150b5a6f623367921a9d186eb755a7c674a18262
Bucky Townsend <atownsend@unluckysandpiper.com>
2014-12-30 14:06:00 -0800

    changed admin to admin/users to manage users

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -47,7 +47,7 @@
 
 	Router::connect('/join/:url', array('controller' => 'users', 'action' => 'join', array('url')));
 	//admin route
-	Router::connect('/admin', array('controller' => 'users', 'action' => 'index'));
+	Router::connect('/admin/users', array('controller' => 'users', 'action' => 'index'));
 
 /**
  * Load all plugin routes. See the CakePlugin documentation on

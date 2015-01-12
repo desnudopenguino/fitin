0bee7446d6d9e4258d90839be3de2b70fa6fd563
Bucky Townsend <atownsend@unluckysandpiper.com>
2015-01-12 14:56:06 -0800

    added exit to break out hopefully

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -323,6 +323,9 @@ class UsersController extends AppController {
 // the checkout function, that loads the account types and levels and stuff.
 	public function checkout() {
 		debug($this->request->data);
+		if($this->request->is('post')) {
+			exit;	
+		}
 	}
 }
 ?>

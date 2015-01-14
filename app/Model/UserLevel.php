b7863501f0ad3db2571ed78170d0fff72d472e1c
Bucky Townsend <atownsend@unluckysandpiper.com>
2015-01-14 15:48:46 -0800

    added functionality to find the plans to change to

--- app/Model/UserLevel.php
+++ app/Model/UserLevel.php
@@ -6,5 +6,36 @@ Class UserLevel extends AppModel {
 	
 	public $hasMany = array(
 		'User');
+
+	public function findPlans() {
+		switch(Authcomponent::user('role_id')) {
+			case 1:
+				return $this->findEmployerPlans();
+				break;
+			case 2:
+				return $this->findApplicantPlans();
+				break;
+		}
+	}
+
+	public function findApplicantPlans() {
+		return $this->find('list' array(
+			'conditions' => array(
+				'id' => '>= 20',
+				'id' => '< 30'),
+			'fields' => array(
+				'UserLevel.stripe_plan',
+				'User.description'. ' $'.'User.price')));
+	}
+	
+	public function findEmployerPlans() {
+		return $this->find('list' array(
+			'conditions' => array(
+				'id' => '>= 10',
+				'id' => '< 20'),
+			'fields' => array(
+				'UserLevel.stripe_plan',
+				'User.description'. ' $'.'User.price')));
+	}
 }
 ?>

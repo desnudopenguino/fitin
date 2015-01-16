4011593f16172f4d6eeab75e19efc62a4c172d22
Bucky Townsend <atownsend@unluckysandpiper.com>
2015-01-15 17:11:18 -0800

    added functions to find all ids and find all premium ids for applicants for the search comparison stuff

--- app/Model/Applicant.php
+++ app/Model/Applicant.php
@@ -231,5 +231,21 @@ Class Applicant extends AppModel {
 					'PhoneNumber'
 						))));
 	}
+
+	public function findAllIds() {
+		return $this->find('all', array(
+			'fields' => array(
+				'Applicant.user_id')));
+	}
+	
+	public function findAllPremiumIds() {
+		return $this->find('all', array(
+			'fields' => array(
+				'Applicant.user_id'),
+			'contain' => array(
+				'User' => array(
+					'conditions' => array(
+						'User.user_level_id > ' => 20)))));
+	}
 }
 ?>

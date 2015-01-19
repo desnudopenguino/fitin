538290a6e9a56af944b31b081951f3fbd34ecdb6
Bucky Townsend <atownsend@unluckysandpiper.com>
2015-01-18 18:40:10 -0800

    positions_return not positions

--- app/Model/Company.php
+++ app/Model/Company.php
@@ -75,7 +75,7 @@ Class Company extends AppModel {
 				$positions_return[] = array('Position' => array('id' => $position['id'], 'employer_id' => $position['employer_id']));
 			}
 		}
-debug($positions);
+debug($positions_return);
 		return $positions_return;
 	}
 }

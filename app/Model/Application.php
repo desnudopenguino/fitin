17f7c6a7670b2210790c64d75f4e8fd22017d085
Bucky Townsend <atownsend@unluckysandpiper.com>
2015-01-26 07:37:18 -0800

    removed unset to see what dumbness in happening

--- app/Model/Application.php
+++ app/Model/Application.php
@@ -69,7 +69,7 @@ Class Application extends AppModel {
 		
 		foreach($applications as $aKey => $application) {
 			if($applications[$aKey]['Application']['Position'] == null) {
-				unset($applications[$aKey]);
+//				unset($applications[$aKey]);
 			} else {
 				$applications[$aKey]['Application']['Applicant'] = $application['Applicant'];
 				$applications[$aKey]['Application']['Position'] = $application['Position'];

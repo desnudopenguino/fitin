333636ef6a7177f9c1a1b6b6d57341a6ece6130e
Bucky Townsend <atownsend@unluckysandpiper.com>
2015-01-28 08:07:38 -0800

    added test and live secret keys

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -73,6 +73,7 @@ Cache::config('default', array('engine' => 'File'));
 //BoostCake Bootstrap plugin
 CakePlugin::load('BoostCake');
 CakePlugin::load('Stripe');
+Configure::write('Stripe.LiveSecret', 'sk_test_8AzqUrKRSP4sZs7bH0xfRPCC');
 Configure::write('Stripe.TestSecret', 'sk_live_ucgqXgv5mlANB5QnMrjSHK8J');
 Configure::write('Stripe.mode', 'Live');
 Configure::write('Stripe.currency', 'usd');

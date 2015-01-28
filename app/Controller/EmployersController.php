c852fe0e6a59e5d20b93491d16a43c8e41fe4cfd
Bucky Townsend <atownsend@unluckysandpiper.com>
2015-01-28 10:53:14 -0800

    added user_id to the employer object to be saved, so it saves it correctly

--- app/Controller/EmployersController.php
+++ app/Controller/EmployersController.php
@@ -73,6 +73,7 @@ class EmployersController extends AppController {
 				unset($this->request->data['Organization']);
 				$this->request->data['Employer']['organization_id'] = $organization['Organization']['id'];
 				$employer = $this->request->data['Employer'];
+				$employer['user_id'] = $id;
 				unset($this->request->data['Employer']);
 				$this->Employer->User->saveAll($this->request->data, array('validation' => false));
 				$employer['user_id'] = $this->Employer->User->getLastInsertID();

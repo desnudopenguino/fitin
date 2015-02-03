a64f80ce5d2cb03dd9f5cf4f5b70684af2ff6a3a
Bucky Townsend <atownsend@unluckysandpiper.com>
2015-02-03 12:11:28 -0800

    added logic to remove validation from organization object, and prevent company and organization data from being saved to db if organization data is not in the request data

--- app/Controller/EmployersController.php
+++ app/Controller/EmployersController.php
@@ -188,18 +188,25 @@ class EmployersController extends AppController {
 		$employer = $this->Employer->findEdit($id);
 		$this->set('employer', $employer);
 
+		if($employer['User']['user_level_id'] == 17) {
+			$this->Organization->validator()->remove('organization_name');
+		}
+
 		if($this->request->is('post') || $this->request->is('put')) { 
 			if($this->Employer->User->saveAll($this->request->data, array('validation' => 'only'))) {
-				$organization = $this->Organization->checkAndCreate($this->request->data,1);
-				$this->request->data['Employer']['organization_id'] = $organization['Organization']['id'];
-				unset($this->request->data['Organization']);
-				$this->request->data['Employer']['organization_id'] = $organization['Organization']['id'];
+				if(!empty($this->request->data['Organization'])) {
+					$organization = $this->Organization->checkAndCreate($this->request->data,1);
+					$this->request->data['Employer']['organization_id'] = $organization['Organization']['id'];
+					unset($this->request->data['Organization']);
+				}
 				$employer = $this->request->data['Employer'];
 				unset($this->request->data['Employer']);
 				$this->Employer->User->saveAll($this->request->data, array('validation' => false));
 				$employer['user_id'] = $id;
 				$this->Employer->save($employer);
-				$this->Employer->Company->checkAndCreate($organization);
+				if(!isset($organization)) {
+					$this->Employer->Company->checkAndCreate($organization);
+				}
 				$this->redirect(array('controller' => 'employers', 'action' => 'profile'));
 			} else {
 				$this->Session->setFlash(__('The Employer Information has not been saved'),

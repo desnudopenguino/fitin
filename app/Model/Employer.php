302cc623ba0ee95439679d6dd020448fb514d9be
Bucky Townsend <atownsend@unluckysandpiper.com>
2015-02-03 08:51:46 -0800

    forgot the =>

--- app/Model/Employer.php
+++ app/Model/Employer.php
@@ -83,7 +83,7 @@ Class Employer extends AppModel {
 
 		$return_departments = $departments['Company']['Organization']['Employer'];
 
-		foreach($return_departments as $dKey $dept) {
+		foreach($return_departments as $dKey => $dept) {
 			if(empty($dept['User'])) {
 				unset($return_departments[$dKey]);
 			}

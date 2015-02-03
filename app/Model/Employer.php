e922c313eee6f3b1edc75a95a1fcd670d60490be
Bucky Townsend <atownsend@unluckysandpiper.com>
2015-02-03 09:00:49 -0800

    also remove the owner from the list, so that two updates don't have to happen on the db.

--- app/Model/Employer.php
+++ app/Model/Employer.php
@@ -84,7 +84,7 @@ Class Employer extends AppModel {
 		$return_departments = $departments['Company']['Organization']['Employer'];
 
 		foreach($return_departments as $dKey => $dept) {
-			if(empty($dept['User'])) {
+			if(empty($dept['User']) || $dept['User']['id'] == $id) {
 				unset($return_departments[$dKey]);
 			}
 		}

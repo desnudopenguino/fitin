6604a84c5d9e898b4fa8d8f9617749bd16d4f9e9
Bucky Townsend <atownsend@unluckysandpiper.com>
2015-02-10 11:03:53 -0800

    added ajax form submission

--- app/webroot/js/applicant_search.js
+++ app/webroot/js/applicant_search.js
@@ -17,3 +17,17 @@ console.log(id);
 	return false;
 });
 
+
+$(document).on('submit',"#searchApplicantForm", function() {
+
+	$.ajax({
+		url: $(this).attr('action'),
+		type: $(this).attr('method'),
+		async: true,
+		data: $(this).serialize(),
+		success: function(result) {
+			$('#results').html(result).hide().fadeIn(300);
+		}});
+		
+	return false;
+});

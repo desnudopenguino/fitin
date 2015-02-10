4f44bf68e935027a1891bfd1a69df69eb1ca234f
Bucky Townsend <atownsend@unluckysandpiper.com>
2015-02-10 10:17:36 -0800

    added default results to employer search

--- app/Controller/EmployersController.php
+++ app/Controller/EmployersController.php
@@ -270,18 +270,28 @@ class EmployersController extends AppController {
 		if(!empty($position_id)) {
 
 			$positionCard = $this->Employer->Position->loadDataCard($position_id);
-			if($this->Auth->user('user_level_id') == 10) {
-        $applicants = $this->Applicant->findPremiumIds($user_id, array('distance' => 25, 'scale' => 3959));
-			} else {
-        $applicants = $this->Applicant->findAllIds($user_id, array('distance' => 25, 'scale' => 3959));
-			}
 
+			$search = array('distance' => 25,
+				'scale' => 3959,
+				'job' => 50,
+				'culture' => 20);
+
+			if($this->Auth->user('user_level_id') == 10) {
+        $applicants = $this->Applicant->findPremiumIds($user_id, array('distance' => $search['distance'], 'scale' => $search['scale']));
+			} else {
+        $applicants = $this->Applicant->findAllIds($user_id, array('distance' => $search['distance'], 'scale' => $search['scale']));
+			}
+		
 			$applicantCards = array();
 			foreach($applicants as $applicant) {
 				$applicantCard = $this->Applicant->loadDataCard($applicant['Applicant']['user_id']);
 				$applicantCard['Results'] = $this->DataCard->compare($applicantCard,$positionCard);
-				$applicantCard['Culture'] = $this->Employer->User->UserCultureAnswer->compareCulture($applicant['Applicant']['user_id'],$this->Auth->user('id'));
-				$applicantCards[] = $applicantCard;
+				if($applicantCard['Results']['percent'] >= $search['job']) {
+					$applicantCard['Culture'] = $this->UserCultureAnswer->compareCulture($applicant['Applicant']['user_id'],$this->Auth->user('id'));
+					if($applicantCard['Culture']['Total']['percent'] >= $search['culture']) {
+						$applicantCards[] = $applicantCard;
+					}
+				}
 			}
 			$applicantCards = $this->DataCard->sortByJobMatch($applicantCards);	
 			$this->set('position_card', $positionCard);

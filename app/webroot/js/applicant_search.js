6d1465b8a829aa23c00d9db9284c507222a1c073
Bucky Townsend <atownsend@unluckysandpiper.com>
2015-02-10 10:59:30 -0800

    added applicant_search.js

--- app/webroot/js/applicant_search.js
+++ app/webroot/js/applicant_search.js
@@ -1,19 +1,14 @@
 //load the mail stuff if inbox button is clicked
-$(document).on('click',".apply", function(e) {
+$(document).on('submit',"#searchApplicantForm", function() {
 
-console.log('Applied to '+ $(this).attr('href'));
-e.preventDefault();
-var id = '#'+$(this).attr('id');
-console.log(id);
 	$.ajax({
-		url: $(this).attr('href'),
-		type: 'POST',
+		url: $(this).attr('action'),
+		type: $(this).attr('method'),
 		async: true,
-		success: function() {
-			$(id).addClass('disabled');
-			$(id).html('<i class="glyphicon glyphicon-send"></i> Applied');
-			$(id).parent().parent().parent().parent().parent().parent().parent().prepend('<div class="alert alert-success alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button><strong>Success!</strong> You have applied to this position.</div>');
+		data: $(this).serialize(),
+		success: function(result) {
+			$('#results').html(result).hide().fadeIn(300);
 		}});
+		
 	return false;
 });
-

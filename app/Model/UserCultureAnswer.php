ae6ae443cf96967870ef444b1e870c5754b8a009
Bucky Townsend <atownsend@unluckysandpiper.com>
2015-02-25 13:06:38 -0800

    tweaking the algo to get the total count for each question type

--- app/Model/UserCultureAnswer.php
+++ app/Model/UserCultureAnswer.php
@@ -39,7 +39,9 @@ debug($employerCulture);
 debug($applicantCulture);
 
 		foreach($employerCulture as $qKey => $question) {
-			$culture[$question['CultureQuestion']['culture_question_type_id']]['total'] = $culture[$question['CultureQuestion']['culture_question_type_id']]['total'] +1;
+			$question_type = $quesiton['CultureQuestion']['culture_question_type_id'];
+debug($question_type);
+			$culture[$question_type]['total'] = $culture[$question_type]['total'] + 1;
 //			foreach($culture as $cKey => $cval) {
 //				if($cKey == $question['CultureQuestion']['culture_question_type_id']) {
 //					$culture[$cKey]['total'] = $culture[$cKey]['total'] + 1;
